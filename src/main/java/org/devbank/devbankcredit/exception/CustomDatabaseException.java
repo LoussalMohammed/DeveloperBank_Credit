42e8eea63b65d94a63cc05510be253327212b362
loussalmohammed <hplaptopsimo@gmail.com>
2024-10-07 14:34:39 +0100

    add custom database and validation exception classes

---
+++ src/main/java/org/devbank/devbankcredit/exception/CustomDatabaseException.java
@@ -0,0 +1,7 @@
+package org.devbank.devbankcredit.exception;
+
+public class CustomDatabaseException extends RuntimeException {
+    public CustomDatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
